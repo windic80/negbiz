22d6580800e4a045cd2e0afa3536b6738d2d8dd9
windic80 <windic80@naver.com>
2024-05-09 09:41:40 +0900

    commit

---
+++ src/main/java/kr/or/egbiz/negbiz/NegbizApplication.java
@@ -0,0 +1,13 @@
+package kr.or.egbiz.negbiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegbizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegbizApplication.class, args);
+	}
+
+}
